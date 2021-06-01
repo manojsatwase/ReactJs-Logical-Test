50970c8ba9cb6bd8fe086cc6f8bfd575cbeb4a01
manojsatwase <33016614+manojsatwase@users.noreply.github.com>
2021-06-01 20:47:12 +0530

    Add files via upload

---
+++ js/Even_Numbers_Of_Array.js
@@ -0,0 +1,15 @@
+// Question 1
+// Define an array of numbers (use any random numbers). Write a program to print only the even numbers of the array. Do not use any library functions, need to do via for loops only
+
+const Even_Numbers_Of_Array = (function () {
+ let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+ let eventNum = []
+ for (let i = 0; i < arr.length; i++) {
+  if (i % 2 === 0) {
+   eventNum.push(arr[i])
+  }
+ }
+ return eventNum;
+})
+
+export default Even_Numbers_Of_Array;
---
+++ js/app.js
@@ -0,0 +1,11 @@
+import Even_Numbers_Of_Array from "./Even_Numbers_Of_Array.js";
+import FindMaxConsecutiveOnes from "./findMaxConsecutiveOnes.js";
+import Unique_Number from "./Unique_Number.js";
+
+
+
+Even_Numbers_Of_Array()
+console.log(Unique_Number());
+console.log(FindMaxConsecutiveOnes([0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0]))
+
+
